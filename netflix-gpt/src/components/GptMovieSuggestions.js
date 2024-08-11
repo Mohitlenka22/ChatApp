7ade640af6d435545bc4d174acb050b4490ea1b4
LENKA MOHIT PAVAN KALYAN <mohitlenka865@gmail.com>
2024-08-11 15:18:31 +0530

    Delete netflix-gpt directory

--- netflix-gpt/src/components/GptMovieSuggestions.js
+++
@@ -1,7 +0,0 @@
-const GptMovieSuggestions = () => {
-  return <div>
-    
-  </div>;
-};
-
-export default GptMovieSuggestions;
--- netflix-gpt/src/utils/geminiai.js
+++
@@ -1,8 +0,0 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import { GEMINIAPI_KEY } from './constants';
-
-const genAI = new GoogleGenerativeAI(GEMINIAPI_KEY);
-
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
-
-export default model;
